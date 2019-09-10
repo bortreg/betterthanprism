2a9791792ecd7a620daf207adcb7f6448ec231e5
MPW <mwood83@gmail.com>
2019-09-10 15:08:45 -0700

    added dplyr and changed weeks to decimal values to indicate days

--- Rscripts/ovsTfhFlow.R
+++ Rscripts/ovsTfhFlow.R
@@ -1,9 +1,12 @@
 #Load packages
 library(ggplot2)
+library(dplyr)
 
 #Open Data from Exported from FlowJo and format for analysis
 tfh <- read.csv("~/Desktop/20190905_TfhPanel_comb_v01.csv")
 tfh$macaque.ID <- as.factor(tfh$macaque.ID)
+tfh <- tfh %>%
+  mutate(study.wk = replace(study.wk, study.wk == 16, 16.43))
 
 #Index tfh to identify populations of interest
 cd8Cxcr5 <- tfh[tfh$population == "Cells/Singlets/Live/Tcells/CD4-,CD8+/CXCR5+", ]
@@ -18,4 +21,7 @@ wk2 <- cd4pd1X5[cd4pd1X5$study.wk == -2, ]
 #plot data for each population at a given timepoint
 p <- ggplot(cd4pd1Dpos, aes(group = study.wk, x = study.wk, y = statistic)) +
   geom_boxplot(width = 1.8, fill = "white") + theme_light()
-p + geom_jitter(width = 0.08, aes(color = macaque.ID, shape = vaccine.route))
+wkP <- p + geom_jitter(width = 0.08, aes(shape = vaccine.route, color = macaque.ID))
+wkP + geom_vline(xintercept = 0, linetype="solid", color = "black", size=0.3)+ 
+  geom_vline(xintercept=8, linetype="dashed", color = "black", size = 0.3)+
+  geom_vline(xintercept=16, linetype="dashed", color = "black", size = 0.3)
