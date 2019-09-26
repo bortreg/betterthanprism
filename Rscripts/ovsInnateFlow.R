544f66555ea0a250b14993635c8789dae3f15e35
MPW <mwood83@gmail.com>
2019-09-26 12:28:55 -0700

    ovsInnateFlow: Deleted line graph code**

--- Rscripts/ovsInnateFlow.R
+++ Rscripts/ovsInnateFlow.R
@@ -40,28 +40,3 @@ dev.off()
 
 
 
-#Graph a test population
-DC83_InnFlow <- rawInnFlow[rawInnFlow$cell.population == "Cells/Singlets/HLADR+CD3-/Bcells/CD11c+", ]
-DC83_PBMC <- DC83_InnFlow[DC83_InnFlow$tissue == "PBMC", ]
-ggplot(DC83_PBMC, aes(x = study.day, y = statistic, color = macaque.ID, group = macaque.ID)) + 
-  geom_point() + geom_line()
-
-#Graph Multiple Tissues at once
-lg <- ggplot(DC83_InnFlow, aes(x = study.day, y = statistic, 
-                         color =  macaque.ID, 
-                         shape =  vacc.route,
-                         linetype = vacc.route,
-                         group = macaque.ID)) + 
-  scale_shape_manual(name = "Vaccine Route", values = c(19,1)) +
-  geom_point() + geom_line() +
-  facet_grid(~tissue, scales = "free_x", space = "free_x") +
-  geom_vline(xintercept = 0, linetype="solid", color = "black", size=0.3)+ 
-  geom_vline(xintercept=56, linetype="dashed", color = "black", size = 0.3)+
-  geom_vline(xintercept=112, linetype="dashed", color = "black", size = 0.3)
-
-#Add plot labels
-lg + labs(title = "CD83+ mDC", color = "macaque")
-
-
-
-
