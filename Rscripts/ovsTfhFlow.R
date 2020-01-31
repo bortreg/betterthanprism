b67b13d5cb58512fc0a20ed7a06fb757ea760d1d
MPW <mwood83@gmail.com>
2020-01-31 12:57:50 -0800

    pathwayCorrelation.R

--- Rscripts/ovsTfhFlow.R
+++ Rscripts/ovsTfhFlow.R
@@ -16,30 +16,8 @@ cd4pd1Dneg <- tfh[tfh$population == "Cells/Singlets/Live/Tcells/CD4+,CD8-/PD-1lo
 cd4pd1X5 <- tfh[tfh$population == "Cells/Singlets/Live/Tcells/CD4+,CD8-/PD-1low/CXCR3-,CXCR5+", ]
 cd4pd1X3 <- tfh[tfh$population == "Cells/Singlets/Live/Tcells/CD4+,CD8-/PD-1low/CXCR3+,CXCR5-", ]
 cd4pd1Dpos <- tfh[tfh$population == "Cells/Singlets/Live/Tcells/CD4+,CD8-/PD-1low/CXCR3+,CXCR5+", ]
-cd4pd1X5_I <- tfh[tfh$population == "Cells/Singlets/Live/Tcells/CD4+,CD8-/PD-1low/CXCR3+,CXCR5+", ]
-
-#plot data for each population at a given timepoint
-p <- ggplot(cd4pd1X5, aes(group = study.wk, x = study.wk, y = statistic)) +
-  geom_boxplot(size = 0.6, width = 0.6, fill = "lightgray") + theme_light()
-wkP <- p + geom_jitter(width = 0.08, aes(shape = vaccine.route, color = macaque.ID))
-wkPl <- wkP + geom_vline(xintercept = 0, linetype="solid", color = "black", size=0.3)+ 
-  geom_vline(xintercept=8, linetype="dashed", color = "black", size = 0.3)+
-  geom_vline(xintercept=16, linetype="dashed", color = "black", size = 0.3)
-
-wkPl
-
-#plot 2 time points for a given population
-wkA <- cd4pd1X5[cd4pd1X5$study.wk == 8, ]
-wkB <- cd4pd1X5[cd4pd1X5$study.wk == 8.43, ] 
-p <- ggplot(wkA, aes(group = study.wk, x = study.wk, y = statistic)) +
-  geom_boxplot(size = 0.3, width = 1.8, fill = "white") + theme_light()
-wkP <- p + geom_jitter(width = 0.08, aes(shape = vaccine.route, color = macaque.ID))
-wkPl <- wkP + geom_vline(xintercept = 0, linetype="solid", color = "black", size=0.3)
-
-#Add plot labels
-wkPlab <- wkPl + labs(title = "Circulating Tfh",subtitle = "PBMC", color = "macaque",
-                   x = "Study Week", y = "%CXCR3- CXCR5+ of PD-1low CD4 T cells")
-wkPlab  
+cd4pd1X5_I <- tfh[tfh$population == "Cells/Singlets/Live/Tcells/CD4+,CD8-/PD-1low/CXCR3-,CXCR5+/ICOS+", ]
+cd4pd1X5_M <- tfh[tfh$population == "Cells/Singlets/Live/Tcells/CD4+,CD8-/PD-1low/CXCR3-,CXCR5+/Maf+", ]
 
 #Sort data by macaque ID for paired comparisons
 cd8Cxcr5 <- cd8Cxcr5[order(cd8Cxcr5$macaque.ID), ]
@@ -47,17 +25,38 @@ cd4pd1Dneg <- cd4pd1Dneg[order(cd4pd1Dneg$macaque.ID), ]
 cd4pd1X5 <- cd4pd1X5[order(cd4pd1X5$macaque.ID), ]
 cd4pd1X3 <- cd4pd1X3[order(cd4pd1X3$macaque.ID), ]
 cd4pd1Dpos <- cd4pd1Dpos[order(cd4pd1Dpos$macaque.ID), ]
+cd4pd1X5_M <- cd4pd1X5_M[order(cd4pd1X5_M$macaque.ID), ]
+cd4pd1X5_I <- cd4pd1X5_I[order(cd4pd1X5_I$macaque.ID), ]
+
+#determine averages for for populations of interest by group
+PoI <- cd4pd1X5_I
+PoI_IM <- PoI[PoI$vaccine.route == "IM", ]
+PoI_IEP <- PoI[PoI$vaccine.route == "Oral", ]
+
+
+#plot data for each population at a given timepoint
+p <- ggplot(PoI, aes(group = study.wk, x = study.wk, y = statistic)) +
+  geom_boxplot(size = 0.4, width = 0.4, fill = "lightgray") + theme_classic() 
+wkP <- p + geom_jitter(width = 0.08, aes(shape = vaccine.route, color = macaque.ID))
+wkPl <- wkP + geom_vline(xintercept = 0, linetype="solid", color = "black", size=0.3)+ 
+  geom_vline(xintercept=8, linetype="dashed", color = "black", size = 0.3)+
+  geom_vline(xintercept=16, linetype="dashed", color = "black", size = 0.3)
+
+wkPl
+
+#Add plot labels
+wkPlab <- wkPl + labs(title = "Circulating Tfh",subtitle = "PBMC", color = "macaque",
+                   x = "Study Week", y = "%ICOS+ of CXCR3- cTfh cells")
+wkPlab  
 
 
 #Asign variables for column statistics for timepoints
-col1 <- cd4pd1Dpos[cd4pd1Dpos$study.wk == 8, ]
+col1 <- cd4pd1X5_I[cd4pd1X5_I$study.wk == -2, ]
 col1oral <- col1[col1$vaccine.route == "Oral", ]
 col1im <- col1[col1$vaccine.route == "IM", ]
-col2 <- cd4pd1Dpos[cd4pd1Dpos$study.wk == 8.43, ]
+col2 <- cd4pd1X5_I[cd4pd1X5_I$study.wk == 2, ]
 col2oral <- col2[col2$vaccine.route == "Oral", ]
 col2im <- col2[col2$vaccine.route == "IM", ]
-
-
 
 #Check normality of data (data is normal if p > 0.05)
 shapiro.test(col1$statistic)
@@ -81,8 +80,8 @@ qqnorm(col2im$statistic,main = "Column 1", pch=19)
 qqline(col2im$statistic)
 
 #ttest of data
-t.test(col1$statistic, col2$statistic, paired = FALSE)
-t.test(col1im$statistic, col2im$statistic, paired = FALSE)
-t.test(col1oral$statistic, col2oral$statistic, paired = FALSE)
+t.test(col1$statistic, col2$statistic, paired = TRUE)
+t.test(col1im$statistic, col2im$statistic, paired = TRUE)
+t.test(col1oral$statistic, col2oral$statistic, paired = TRUE)
 
 
