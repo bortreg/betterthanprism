fd1934717eccbc1cacbc71f07f57fb8ea0f7c1bf
MPW <mwood83@gmail.com>
2020-02-07 17:13:40 -0800

    Transform DE data with log2 function rather than indexing Log2 variable

--- Rscripts/diffGenExp_NS.R
+++ Rscripts/diffGenExp_NS.R
@@ -41,16 +41,42 @@ names(HEU_DE) <- gsub("HEU.Gene","Gene",names(HEU_DE))
 
 DEcomb <- merge(HU_DE, HEU_DE)
 DEcomb <- DEcomb[order(DEcomb$HU.Log2.fold.change, decreasing = TRUE), ]
-DEtop100 <- DEcomb[1:100, c("HU.Log2.fold.change","HEU.Log2.fold.change")] 
-row.names(DEtop100) <- DEcomb[1:100,"Gene"]
-DEtop100 <- t(DEtop100)  
+DEtop50 <- DEcomb[1:50, c("HU.Log2.fold.change", "HEU.Log2.fold.change")] 
+row.names(DEtop50) <- DEcomb[1:50,"Gene"]
+DEtop50 <- t(DEtop50)  
+
+#Label DE gene hits
+
+
 
 #Heatmap of DEtop100
-zscore <- function(x) {          #zscore function
-  z <- (mat_data - mean(x)) / sd(x)
-  return(z)
-}
+my_palette<- colorRampPalette(c("#45A242","#5CA25A","#89BE88","#C6E0C5","#CECFCE"))(n = 299)
+tiff("~/Desktop/test.tiff", units = "in", width=4, height=10, res = 300)
+heatmap <- heatmap.2(t(DEtop50[,1:50]), 
+                     col=my_palette, 
+                     Rowv = FALSE, 
+                     dendrogram = "none",
+                     trace = "none",
+                     labCol = FALSE,
+                     margins = c(1,10),
+                     Colv = FALSE,
+                     offsetRow = 0.01,
+                     key.par = list(cex=0.8),
+                     key.title = NA,
+                     key.xlab = "log2 Fold Change",
+                     key.ylab = "count",
+                     ColSideColors = c("#838284", "#9554BE"))
 
+par(lend = 1)
+legend("topright",
+       legend = c("HU", "HEU"),
+       cex = 0.7,
+       pt.cex = 1,
+       col = c("#838284", "#9554BE"),
+       lty = 1,
+       lwd = 10
+)          
+dev.off()
 
 
 #Calculate z scores from ratios of fold change from experimental group over control group
