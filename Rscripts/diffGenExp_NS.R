8fa20259b6a04c30d59c38f8827c6e2e8dd9745b
MPW <mwood83@gmail.com>
2020-02-12 21:50:33 -0800

    Changed color palettes to black grey red

--- Rscripts/diffGenExp_NS.R
+++ Rscripts/diffGenExp_NS.R
@@ -12,11 +12,11 @@ HU_DE <- read.csv("~/Documents/R/Nanostring_DataFrames/DE_HU.csv")
 HEU_DE <- read.csv("~/Documents/R/Nanostring_DataFrames/DE_HEU.csv")
 
 #Plot volcanoes
-df <- HU_DE
+df <- HEU_DE
 pdf("~/Desktop/test.pdf", width=5, height=5)
 ggplot(df, aes(x=Log2.fold.change, y=-log2(P.value), color = FDR)) +
   geom_point(size = 1.5) +
-  scale_color_manual(values = c("#45A242","#5CA25A","#89BE88","#C6E0C5","#CECFCE")) +
+  scale_color_manual(values = c("red","#FE2E2E","#FA5858","#F5A9A9","#BDBDBD")) +
   theme_bw() +
   geom_text_repel(
     data = df[1:50,],
@@ -49,10 +49,10 @@ DEtop50 <- t(DEtop50)
 
 
 
-#Heatmap of DEtop100
+#Heatmap of DEtop50
 DEtop50 <- log2(DEtop50)
-my_palette<- colorRampPalette(c("#E2ECE1","#C6E0C5","#89BE88","#5CA25A","#45A242"))(n = 40.0)
-pdf("~/Desktop/test.pdf", width=3, height = 8)
+my_palette<- colorRampPalette(c("black","gray","red"))(n = 40)
+pdf("~/Desktop/test.pdf", width=5, height = 5)
 heatmap <- heatmap.2(t(DEtop50[,1:50]), 
                      col=my_palette, 
                      Rowv = FALSE, 
