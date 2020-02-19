a525425eb2ccb690331666d832cb3026ea5ccabe
MPW <mwood83@gmail.com>
2020-02-18 22:25:21 -0800

    Revert "Revert "Added 5 additional gene modules for heatmaps of normalized expression data""
    This reverts commit 5520265dd58f0a04239ab1e8813070070af06e95.

--- Rscripts/topPathways.R
+++ Rscripts/topPathways.R
@@ -1,18 +1,18 @@
 library(dplyr)
 library(gplots)
 
-##Import Pathway Scores
+#Import Pathway Scores
 pathScores <- read.csv("~/Desktop/pathMerge.csv")
 pathScores <- pathScores[order(pathScores$HU.MeanBCG, decreasing = TRUE),]
 
-Pathtop50 <- pathScores[1:50, c("HU.MeanBCG","HEU.MeanBCG")] 
+Pathtop50 <- pathScores[1:50, c("HU.MeanUntreat","HU.MeanBCG")] 
 row.names(Pathtop50) <- pathScores[1:50,"Pathway"]
 Pathtop50 <- t(Pathtop50)  
 
 
 #Heatmap of path score top 50
 my_palette<- colorRampPalette(c("#E2ECE1","#C6E0C5","#89BE88","#5CA25A","#45A242"))(n = 40.0)
-pdf("~/Desktop/test.pdf", width=7, height = 10)
+pdf("~/Desktop/test.pdf", width=5, height = 10)
 heatmap <- heatmap.2(t(Pathtop50[,1:50]), 
                      col=my_palette, 
                      Rowv = FALSE, 
@@ -26,14 +26,14 @@ heatmap <- heatmap.2(t(Pathtop50[,1:50]),
                      key.par = list(cex=0.8),
                      key.title = NA,
                      key.xlab = "pathway scores",
-                     ColSideColors = c("#838284", "#9554BE"))
+                     ColSideColors = c("#ECC03F", "#C6C4BF"))
 
 par(lend = 1)
 legend("topright",
-       legend = c("HU", "HEU"),
+       legend = c("Untreated", "BCG"),
        cex = 0.7,
        pt.cex = 1,
-       col = c("#838284", "#9554BE"),
+       col = c("#ECC03F", "#C6C4BF"),
        lty = 1,
        lwd = 10
 )          
