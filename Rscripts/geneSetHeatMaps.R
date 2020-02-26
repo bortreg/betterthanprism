f9a3d38da92eaef5efc06bd94404c0542adaf4f3
MPW <mwood83@gmail.com>
2020-02-25 16:36:16 -0800

    Added condensed Geneset which combines previous slected genesets with removed duplicates; set scaling for scale to correct height to ratio using lhei argument

--- Rscripts/geneSetHeatMaps.R
+++ Rscripts/geneSetHeatMaps.R
@@ -23,11 +23,43 @@ mat_data <- log2(mat_data)
 mat_data <- zscore(mat_data)
 mat_data <- cbind(mat_data, HUnorm[,3])
 
-#Load Gene Sets
-geneSets <- read.csv("~/Documents/R/Nanostring_DataFrames/GeneModuleLists.csv")
+#Load Condensed Gene Set List
+geneSets <- read.csv("~/Documents/R/Nanostring_DataFrames/GeneModuleCond.csv")
 geneSets <- as_tibble(geneSets)
+condGenes <- c(as.character(geneSets$condensed[1:137]), "condition")
+HUnormCond <- mat_data[,condGenes]
+HUnormCond$cc <- ifelse(HUnormCond$condition == "BCG", "#ECC03F", "#2E2EFE")
 
-#Define
+#plot heatmap
+hm_df <- HUnormCond
+pdf("~/Desktop/test3.pdf", width=8, height=((length(hm_df)-2)*0.18))
+heatmap <- heatmap.2(t(hm_df[,1:(length(hm_df)-2)]), 
+                     col=my_palette, 
+                     Rowv = TRUE, 
+                     dendrogram = "row",
+                     trace = "none",
+                     labCol = FALSE,
+                     margins = c(7,12),
+                     offsetRow = 0.01,
+                     keysize = 1.8, 
+                     density.info = "none",
+                     key.par = list(cex=0.8),
+                     key.title = NA,
+                     key.xlab = "z-score",
+                     ColSideColors = HUnormCond$cc,
+                     lhei = c(1,10))
+par(lend = 1)
+legend("topright",
+       legend = c("untreated","BCG"),
+       cex = 0.7,
+       pt.cex = 1,
+       col = c("#2E2EFE","#ECC03F"),
+       lty = 1,
+       lwd = 10
+)             
+dev.off()
+
+#Define Gene Sets
 inflammGenes <- c(as.character(geneSets$inflammatory.response[1:35]), "condition")
 HUnormInflamm <- mat_data[,inflammGenes]
 HUnormInflamm$cc <- ifelse(HUnormInflamm$condition == "BCG", "#ECC03F", "#2E2EFE")
@@ -47,33 +79,4 @@ HUnormCellProlif$cc <- ifelse(HUnormCellProlif$condition == "BCG", "#ECC03F", "#
 nkbCasGenes <- c(as.character(geneSets$Ikk.nfkb.cascade[1:20]), "condition")
 HUnormNkbCas <- mat_data[,nkbCasGenes]
 HUnormNkbCas$cc <- ifelse(HUnormNkbCas$condition == "BCG", "#ECC03F", "#2E2EFE")
-
-#plot heatmap
-hm_df <- HUnormNkbCas
-pdf("~/Desktop/test2.pdf", width=5, height=((length(hm_df)-2)*0.285))
-heatmap <- heatmap.2(t(hm_df[,1:(length(hm_df)-2)]), 
-                     col=my_palette, 
-                     Rowv = FALSE, 
-                     dendrogram = "col",
-                     trace = "none",
-                     labCol = FALSE,
-                     margins = c(5,12),
-                     offsetRow = 0.01,
-                     keysize = 1.6,
-                     density.info = "none",
-                     key.par = list(cex=0.8),
-                     key.title = NA,
-                     key.xlab = "z-score",
-                     ColSideColors = HUnormInflamm$cc)
-
-par(lend = 1)
-legend("topright",
-       legend = c("untreated","BCG"),
-       cex = 0.7,
-       pt.cex = 1,
-       col = c("#2E2EFE","#ECC03F"),
-       lty = 1,
-       lwd = 10
-)          
-dev.off()
   
